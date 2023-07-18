fe833a46be594665f875033445997e3b0fb2aaf6
tonyet <tonyet.funx@gmail.com>
2023-07-18 16:10:04 +0800

    改小馬達接口

--- extensions/cActuators/index.js
+++ extensions/cActuators/index.js
@@ -74,7 +74,7 @@ class cActuatorsExtension {
             PIN: {
               type: ArgumentType.STRING,
               defaultValue: '5',
-              menu: 'sPort' // todo-> 改接A端線, 跟伺服馬達相同
+              menu: 'dPort3' // todo-> 改接A端線, 跟伺服馬達相同
             },
             SPEED: {
               type: ArgumentType.SLIDER,
